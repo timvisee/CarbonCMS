6885ef64d35fd0b0adacb0c7e0af408fd5e9d9bd
Tim Visée <timvisee@gmail.com>
2015-06-11 23:17:56 +0200

    Added autoloader base class

---
+++ src/carbon/core/autoloader/Autoloader.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Autoloader.php
+ * The Autoloader class which takes care of all non-loaded classes and tries to load them when being used.
+ *
+ * @author Tim Visee
+ * @website http://timvisee.com/
+ * @copyright Copyright (c) Tim Visee 2012-2014. All rights reserved.
+ */
+
+namespace carbon\core\autoloader;
+
+use carbon\core\util\ArrayUtils;
+use carbon\core\util\StringUtils;
+
+// Prevent direct requests to this file due to security reasons
+defined('CARBON_CORE_INIT') or die('Access denied!');
+
+/**
+ * Autoloader class
+ *
+ * @package carbon\core\autoloader
+ * @author Tim Visee
+ */
+class Autoloader {
+
+}
