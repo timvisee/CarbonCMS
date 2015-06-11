ab893fe4fbf4a11118361489f47aa97d49ef350e
Tim Visée <timvisee@gmail.com>
2015-06-11 23:14:29 +0200

    Removed all CMS and CORE classes, for a cleanup

--- src/carbon/core/database/driver/mysql/Database.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace carbon\core\database\driver\mysql;
-
-// Prevent direct requests to this file due to security reasons
-defined('CARBON_CORE_INIT') or die('Access denied!');
-
-class Database extends \carbon\core\database\lib\pdo\Database { }
--- src/carbon/core/database/lib/sql/Database.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace carbon\core\database\lib\sql;
-
-// Prevent direct requests to this file due to security reasons
-defined('CARBON_CORE_INIT') or die('Access denied!');
-
-abstract class Database extends \carbon\core\database\Database { }
