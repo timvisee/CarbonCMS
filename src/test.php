746c57f96266cd99b82f2b9fa50e7ede7bfb0392
Tim Visée <timvisee@gmail.com>
2015-06-11 22:45:17 +0200

    Improved and cleaned main test script

--- src/test.php
+++ src/test.php
@@ -1,6 +1,17 @@
 <?php
 
-if(!(defined('CARBON_CORE_TEST') && CARBON_CORE_TEST)):
+/**
+ * This file contains test code which will be executed before Carbon CMS starts. This test code is for development only.
+ */
+
+// Check whether Carbon's test mode is enabled or not
+if(defined('CARBON_CORE_TEST') && CARBON_CORE_TEST) {
+    // Show the test header, and return
+    echo '[Carbon CORE] Test mode activated.' . PHP_EOL;
+    return;
+}
+
+// Print the default execution header
 ?>
 <pre>
 CARBON CORE + CMS | V0.1 PRE-ALPHA | BUILD 20932 | BY TIM VISEE
@@ -10,10 +21,14 @@ CARBON CONSOLE MODE
 
 [Carbon CORE] Loading Carbon CORE and Carbon CMS...
 <?php
+
+// Set some time constants for profiling
 $GLOBALS['carbon_time'] = microtime(true);
 $GLOBALS['carbon_memory'] = memory_get_usage();
+
+// Register a shutdown function to show some profiling status after execution
 register_shutdown_function(function() {
-?>
+    ?>
 <pre>
 ---------------------------------------------------------------
 CARBON EXIT
@@ -22,8 +37,5 @@ CARBON EXIT
 CARBON REQUEST PERFORMANCE REPORT
 MEMORY USAGE: <?=((memory_get_usage() - $GLOBALS['carbon_memory']) / (1024 * 1024)); ?> MB
 PROCESS TIME: <?=(microtime(TRUE) - $GLOBALS['carbon_time']); ?> S
-<?php
+    <?php
 });
-else:
-    echo '[Carbon CORE] Test mode activated.' . PHP_EOL;
-endif;
