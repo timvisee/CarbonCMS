324247b23b7277724f7723634ad01bee01e632f2
Tim Visée <timvisee@gmail.com>
2015-07-10 18:10:24 +0200

    Added support for 'any namespace' in file loader.

--- src/carbon/core/autoloader/loader/FileLoader.php
+++ src/carbon/core/autoloader/loader/FileLoader.php
@@ -17,20 +17,19 @@ defined('CARBON_CORE_INIT') or die('Access denied!');
 
 class FileLoader extends BaseLoader {
 
-    /** @var string The base namespace. */
-    // TODO: An empty string, or a single backslash for no base namespace?
+    /** @var string The base namespace, or an empty string for any namespace. */
     private $namespace;
     /** @var Directory The directory of the namespace. */
     private $dir;
 
-    // TODO: Create a variable for this extension?
+    // TODO: Create a variable to set this extension?
     /** @const string The extension of the files, a prefixed period must be included. */
     const FILE_EXTENSION = '.php';
 
     /**
      * Constructor.
      *
-     * @param string $namespace The base namespace.
+     * @param string $namespace The base namespace, or an empty string for any namespace.
      * @param Directory|string $dir The directory of the namespace.
      *
      * @throws CarbonException Throws if the namespace or directory is invalid.
@@ -51,19 +50,20 @@ class FileLoader extends BaseLoader {
     }
 
     /**
-     * Set the base namespace.
+     * Set the base namespace. If an empty string is given all namespaces will be handled by this loader.
+     * A namespace should be suffixed with a single backslash.
      *
-     * @param string $namespace The base namespace.
+     * @param string $namespace The base namespace, or an empty string for any namespace.
      */
-    // TODO: Define whether the namespace should or may be pre- or suffixed with a backslash.
     public function setNamespace($namespace) {
-        // TODO: Validate the namespace here!
+        // Check whether the namespace is an empty string
+        if(trim($namespace) == '') {
+            $this->namespace = '';
+            return;
+        }
 
-        // Suffix a single backslash
-        $namespace = rtrim($namespace, '\\') . '\\';
-
-        // Set the namespace
-        $this->namespace = $namespace;
+        // Suffix a single backslash, and set the namespace
+        $this->namespace = rtrim($namespace, '\\') . '\\';
     }
 
     /**
@@ -100,6 +100,11 @@ class FileLoader extends BaseLoader {
      * @return bool True if the class name or namespace is in the file loader namespace.
      */
     public function isInNamespace($query) {
+        // Check whether the namespace is empty
+        if($this->namespace == '')
+            return true;
+
+        // Check whether the class name or namespace is in the namespace of the file loader.
         return substr($query, 0, strlen($this->namespace)) == $this->namespace;
     }
 
